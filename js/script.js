fac489234023fcd8e11560b841d7f33e372a7cc0
Gleyson Andrade <gleysonsystec@hotmail.com>
2022-12-03 21:56:53 -0300

    ajuste js

--- js/script.js
+++ js/script.js
@@ -4,34 +4,7 @@ $(function(){
 	$('nav.mobile').click(function(){
 		// o que vai acontecer quando clicar na nav mobile
 		var listaMenu = $('nav.mobile ul');
-		//Abrir menu com o fadein
-		//verifica se o menu está aberto ou fechado
 
-		// if(listaMenu.is(':hidden') == true){
-		// 	listaMenu.fadeIn();
-		// }
-		// else{
-		// 	listaMenu.fadeOut();
-		// }
-
-		//Abrir ou fechar sem efeitos
-		// if(listaMenu.is(':hidden') == true){
-		// 	listaMenu.show();
-		// }
-		// else{
-		// 	listaMenu.hide();
-		// }
-
-		//Abrir ou fechar sem efeitos com css
-		// if(listaMenu.is(':hidden') == true){
-		// 	listaMenu.css('display', 'block');
-		// }
-		// else{
-		// 	listaMenu.css('display', 'none');
-		// }
-
-		//Abrir ou fechar o menu com efeito
-		//Verificar se menu está aberto ou fechado
 		if(listaMenu.is(':hidden') == true){
 			var icone = $('.botao-menu-mobile').find('i');
 			icone.removeClass('fa-bars');
